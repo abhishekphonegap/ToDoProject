1be71ae878f0109f59da4c06f25b5de3e0c85304
AbhishekDangwal <dangwal.abhishek@gmail.com>
2021-12-26 22:31:42 +0530

    ToDo List Project

---
+++ ToDoTasks/ToDoListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ToDoTasks
+//
+//  Created by Abhishek Dangwal on 26/12/21.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let arrList = ["Abhishek", "Dangwal", "Abhishek"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+    //MARK: - Table View DataSource
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tblListViewCell", for: indexPath)
+        
+        cell.textLabel?.text = arrList[indexPath.row]
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrList.count
+    }
+    
+    //MARK: - Table View Delegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("Selected Row is \(arrList[indexPath.row])...")
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        if tableView.deselectRow(at: indexPath, animated: true) == .che
+    }
+}
+
